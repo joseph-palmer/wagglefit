676d59898f84a89981924bd914fe1859f3104a09
Joe <palmer-2@hotmail.co.uk>
2021-09-30 20:08:57 +0100

    updated figures with working routine. nothing eally changed results wise but we have more confidence in the proportions of scouts coming back (i think)

--- R/utils.R
+++ R/utils.R
@@ -87,61 +87,25 @@ calc_aic <- function(k, ll) {
   return(2 * k - 2 * ll)
 }
 
-#' Run a KS test on the data
-#'
-#' @description Calulctate the ks test stat and p value for a given model
-#' @param x doubleArray Foraging distance data
-#' @param param_est doubleArray The parameter estimates for a model
-#' @param model_type characterArray the name of the model ran
-#' @param pvalue Bool To return the p value (TRUE) or D statistic (FALSE).
-#' Defaults to TRUE.
-#' @importFrom stats ks.test
-#' @importFrom rlang .data
-#' @concept utility
-calc_ks <- function(x, param_est, model_type, pvalue = TRUE) {
-  if (model_type != "all") {
-    param_est <- param_est[-1]
-  }
-  param_est <- param_est[!is.na(param_est)]
-  ccdf_data <- inverse_ccdf(x)
-  ccdf_model <- model_ccdf(x, param_est, model_number_from_model(model_type))
-  if (all(is.nan(ccdf_model))) {
-    print(paste(model_type, "has bad results"))
-    return(NA)
-  }
-  ks <- ks.test(ccdf_data$prob, ccdf_model)
-  if (pvalue) {
-    return(ks$p.value)
-  } else {
-    return(ks$statistic)
-  }
-}
-
 #' Run a bootstapped KS test on the data
 #'
 #' @description Calulctate the ks test stat and p value for a given model
 #' @param x doubleArray Foraging distance data
 #' @param param_est doubleArray The parameter estimates for a model
 #' @param model_type characterArray the name of the model ran
-#' @param pvalue Bool To return the p value (TRUE) or D statistic (FALSE).
-#' Defaults to TRUE.
 #' @importFrom stats ks.test
 #' @importFrom rlang .data
 #' @importFrom Matching ks.boot
 #' @concept utility
-calc_ks_boot <- function(x, param_est, model_type, pvalue = TRUE) {
-  if (model_type != "all") {
-    param_est <- param_est[-1]
-  }
-  param_est <- param_est[!is.na(param_est)]
+calc_ks_boot <- function(x, param_est, model_type) {
   ccdf_data <- inverse_ccdf(x)
   ccdf_model <- model_ccdf(x, param_est, model_number_from_model(model_type))
   if (all(is.nan(ccdf_model))) {
-    print(paste(model_type, "has bad results"))
+    print(paste(model_type, "has NA results"))
     return(NA)
   }
   ks <- ks.boot(ccdf_data$prob, ccdf_model)
-  return(ks$ks.boot.pvalue)
+  return(ks)
 }
 
 #' Calculate likelihoods for a specified parameter for the collective model
