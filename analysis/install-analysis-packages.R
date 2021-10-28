cf5472678695d71940745ae74f3efaf71596bbe1
joseph-palmer <joe.palmer.2019@live.rhul.ac.uk>
2021-10-28 17:27:28 +0100

    added script to search analysis for packages and install if not installed

---
+++ analysis/install-analysis-packages.R
@@ -0,0 +1,16 @@
+library(stringr)
+library_calls <- purrr::map(
+  list.files("analysis/", pattern = "*.Rmd|.R"),
+  ~ {
+    str_match(readLines(paste0("analysis/", .x)), "library\\((.*?)\\)")[,2]
+  }
+)
+
+library_calls <- unlist(library_calls)
+packages <- library_calls[!is.na(library_calls)]
+new.packages <- packages[
+  !(packages %in% installed.packages()[,"Package"])
+]
+if( length(new.packages)) {
+  install.packages(new.packages)
+}
